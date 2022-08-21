fd84001b9890d636e9617d532b6534b8e5ce307e
HansKindberg <hans.kindberg@gmail.com>
2022-08-21 16:02:32 +0200

    Refactoring: moved method to base-class

--- Source/Application/Controllers/BasicCertificateController.cs
+++ Source/Application/Controllers/BasicCertificateController.cs
@@ -5,7 +5,6 @@ using Application.Models;
 using Application.Models.Cryptography;
 using Application.Models.Views.Shared.Forms;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Application.Controllers
@@ -245,35 +244,6 @@ namespace Application.Controllers
 					form.IssuerList.Add(new SelectListItem(certificate.Subject, certificate.Thumbprint, selected));
 				}
 			}
-		}
-
-		protected internal virtual void SortModelState()
-		{
-			var fieldOrder = this.Facade.CertificateFormOptionsMonitor.CurrentValue.FieldOrder;
-			var index = 0;
-			var keyOrderMap = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
-
-			foreach(var key in this.ModelState.Keys)
-			{
-				keyOrderMap.Add(key, fieldOrder.ContainsKey(key) ? fieldOrder[key] : index);
-				index++;
-			}
-
-			var modelState = new ModelStateDictionary();
-
-			foreach(var key in keyOrderMap.OrderBy(item => item.Value).Select(item => item.Key))
-			{
-				// ReSharper disable PossibleNullReferenceException
-				foreach(var modelError in this.ModelState[key].Errors)
-				{
-					// If we keep the key we have, it will sort it with model metadata. We need another key, the index of the sorted entries, to keep the order we want.
-					modelState.AddModelError((modelState.Count + 1).ToString((IFormatProvider)null), modelError.ErrorMessage);
-				}
-				// ReSharper restore PossibleNullReferenceException
-			}
-
-			this.ModelState.Clear();
-			this.ModelState.Merge(modelState);
 		}
 
 		protected internal virtual IEnumerable<string> SplitOnLineBreaks(string value)
