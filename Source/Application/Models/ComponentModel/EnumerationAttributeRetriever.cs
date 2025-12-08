58392fc9cdbf230ef7784bbefc7aa04e380d9cd4
HansKindberg <hans.kindberg@gmail.com>
2025-12-08 09:11:16 +0100

    CS9273 fix

--- Source/Application/Models/ComponentModel/EnumerationAttributeRetriever.cs
+++ Source/Application/Models/ComponentModel/EnumerationAttributeRetriever.cs
@@ -29,8 +29,8 @@ namespace Application.Models.ComponentModel
 
 							foreach(var enumeration in Enum.GetValues<TEnumeration>())
 							{
-								var field = type.GetField(enumeration.ToString());
-								var attribute = field.GetCustomAttribute<TAttribute>(false);
+								var @field = type.GetField(enumeration.ToString());
+								var attribute = @field.GetCustomAttribute<TAttribute>(false);
 								dictionary.Add(enumeration, attribute);
 							}
 
